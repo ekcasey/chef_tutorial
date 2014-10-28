19b2418e8a16e0557f9dc44d1469ad9bc0972c3e
Emily Casey <ecasey@ThoughtWorks.com>
2014-10-27 23:55:41 -0400

    empty recipe, Berksfile

---
+++ recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: ref-app
+# Recipe:: default
+#
+# Copyright 2012, Gap Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+file "/tmp/hello_world.txt" do
+  action :create
+end
+
